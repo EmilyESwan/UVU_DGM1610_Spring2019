0a05d7b9afd9002e3065fc0fee3271d2fcd03c39
Emily Swan <10856816@my.uvu.edu>
2019-01-30 17:08:18 -0700

    Added function and conditional examples

---
+++ 2D Game/Assets/Scripts/Assignments/Functions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour {
+
+	public string weather; 
+
+
+	void Start(){
+		Weather(weather);
+	}
+
+	void Weather(string weatherState){
+
+		if(weatherState == "Sunny"){
+			print("The sun is shining today!");
+		}
+		else if(weatherState == "Raining"){
+			print("It is raining today!");
+		}
+		else if(weatherState == "Windy"){
+			print("It is very windy today!");
+		}
+		else if(weatherState == "Snowing"){
+			print("It is snowing today!");
+		}
+		else if(weatherState == "Foggy"){
+			print("It is very foggy today!");
+		}
+		else if(weatherState == "Canadian"){
+			print("Eh?");
+		}
+		else{
+			print("I don't understand "+ weatherState);
+		}
+
+	}
+}
---
+++ 2D Game/Assets/Scripts/Assignments/Variables.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour {
+	// Variables
+	public static int Number;
+	public float speed;
+	public string Name;
+
+	// Use this for initialization
+	void Start () {
+		Number = 10;
+		speed = 0.1918f;
+		Name = "Bob";
+
+		print (Name +" is "+Number+" years old!"+ speed); 
+
+	}
+	/*Multi
+	Line 
+	Comment */
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(Number,speed,0);
+	}
+}
