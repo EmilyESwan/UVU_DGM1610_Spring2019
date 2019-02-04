246686d5b1f03fabbfe1a97c7dc7b3d838e5f972
Emily Swan <10856816@my.uvu.edu>
2019-02-04 16:31:27 -0700

    Created Script1

---
+++ 2D Game/Assets/Scripts/Assignments/Script1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script1 : MonoBehaviour {
+
+	public string favoriteFood; 
+
+
+	void Start(){
+		print("My favorite food is "+favoriteFood);
+		Favorite(favoriteFood);
+	}
+
+	void Favorite(string favoriteFood){
+
+		if(favoriteFood == "Ice cream"){
+			print("Perfect for a hot day!");
+		}
+		else if(favoriteFood == "Pizza"){
+			print("Mmmm, Pizza!");
+		}
+		else if(favoriteFood == "Jello"){
+			print("Oooh, Jiggly!");
+		}
+		else if(favoriteFood == "the Souls of the Unborn"){
+			print("Are you Satan?");
+		}
+		else if(favoriteFood == "Chicken Nuggets"){
+			print("Yummy!");
+		}
+		else if(favoriteFood == "Beets"){
+			print("What is wrong with you?!");
+		}
+		else 
+			print("That's silly!");
+	}
+}
