5a56e9035770d58d9ac199fd2333bc079808cec2
Emily Swan <10856816@my.uvu.edu>
2019-02-11 17:19:54 -0700

    Created Tiger scripts & coin pick up

---
+++ 2D Game/Assets/Scripts/Assignments/Tiger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiger : Feline {
+
+	public bool banded;
+
+	void Start () {
+		print("Roar!");
+	}
+	
+	void Update () {
+		
+	}
+}
