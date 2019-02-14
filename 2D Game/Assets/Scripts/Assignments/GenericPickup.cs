d1ef36496760e3c3c66ee0c1dc3ecf609d3c0f96
Emily Swan <10856816@my.uvu.edu>
2019-02-13 17:16:06 -0700

    PlayerController

--- 2D Game/Assets/Scripts/Assignments/GenericPickup.cs
+++ 2D Game/Assets/Scripts/Assignments/GenericPickup.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GenericPickup : MonoBehaviour {
+	
+	public bool isVisable;
+	public int value;
+	public float timer;
 
 	// Use this for initialization
 	void Start () {
