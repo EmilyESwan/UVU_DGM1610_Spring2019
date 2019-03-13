b1cd36687bb788e3c3ded1baf9be1b41820de672
Emily <10856816@my.uvu.edu>
2019-03-12 19:57:28 -0600

    Practice Arrays and Lists

---
+++ 2D Game/Assets/Scripts/Assignments/Practice/Array.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array : MonoBehaviour {
+	
+	//Girl Array
+	public string girl1 = "Michelle";
+	public string girl2 = "Jennifer";
+	public string girl3 = "Diane";
+	public string girl4 = "Stacy";
+	public string girl5 = "Erin";
+	public string girl6 = "Haley";
+	
+	public string[] girls = new string[]{"Michelle","Jennifer","Diane","Stacy","Erin","Haley"};
+
+	//Boy Array
+	public string boy1 = "Jerry";
+	public string boy2 = "Kyle";
+	public string boy3 = "George";
+	public string boy4 = "Larry";
+	public string boy5 = "Mason";
+	public string boy6 = "Bob";
+
+	public string[] boys = new string[]{"Jerry","Kyle","George","Larry","Mason","Bob"};
+
+	// Use this for initialization
+	void Start () {
+		
+		print(boys[3]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ 2D Game/Assets/Scripts/Assignments/Practice/Lists.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lists : MonoBehaviour {
+
+	public List<string> groceries = new List<string> ();
+
+	// Use this for initialization
+	void Start () {
+		groceries.Add("Milk");
+		groceries.Add("Eggs");
+		groceries.Add("Candy");
+		groceries.Add("Toilet Paper");
+		groceries.Add("Potatoes");
+		groceries.Add("Cheese");
+		groceries.Add("Lettuce");
+		groceries.Remove("Candy");
+		groceries.Remove("Potatoes");
+
+		print(groceries[6]); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
