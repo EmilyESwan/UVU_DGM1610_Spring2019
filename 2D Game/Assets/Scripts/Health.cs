40e77953e69293d05fa382c95d4ca6c6e91863c5
Emily <10856816@my.uvu.edu>
2019-03-14 20:52:55 -0600

    Unfinished Health script, and added enemy

---
+++ 2D Game/Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public static int health;
+
+	Text healthText;
+	// Use this for initialization
+	void Start () {
+		healthText = GetComponent<Text>();
+
+		health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0)
+		// go to game over scene
+		
+		healthText.text = " " + health;
+		
+		}
+
+	public static void AddPoints (int pointsToRemove){
+		health -= pointsToRemove;
+	}
+		
+	
+}
