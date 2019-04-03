af3226c468fa1df4a85dd2fd676c94142dbf4ad6
Emily Swan <10856816@my.uvu.edu>
2019-04-03 17:14:56 -0600

    Dictionary, Particles, and enemy trigger

---
+++ 2D Game/Assets/Scripts/Assignments/Dictionaries.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dictionaries : MonoBehaviour {
+
+	public Hashtable personalDetails = new Hashtable();
+	// Use this for initialization
+	void Start () {
+		
+		personalDetails.Add("firstName","Greg");
+		personalDetails.Add("lastName","Lukosek");
+		personalDetails.Add("gender","male");
+		personalDetails.Add("isMarried", true);
+		personalDetails.Add("age", 29);
+
+		print((string)personalDetails["firstName"]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
