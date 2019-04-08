ba006234dd2e148abfb8cf7adf993bce350d37c9
Emily <10856816@my.uvu.edu>
2019-04-08 15:17:12 -0600

    added tree

--- 2D Game/Assets/Scripts/Assignments/Practice/Loop Practice.cs
+++ 2D Game/Assets/Scripts/Assignments/Practice/Loop Practice.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class LoopPractice : MonoBehaviour {
 
+
 	//For Loops
 	// for(int i=24 || int i=16){
 	// 	print (i+ " is divisible by 8");
 	// }
+
+	// for(int i >= 9001){
+	// 	print("It's over 9000!");
+	// }
+
+	// for(int i=0){
+	// 	print("Do not divide by " + i);
+	// }
+	
 		
 	
 }
