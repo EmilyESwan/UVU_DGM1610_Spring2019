1b08178062043a6ec1682033c76a625ccd7a2a2d
Emily <10856816@my.uvu.edu>
2019-04-18 01:54:43 -0600

    Added Sprite Sheet

--- 2D Game/Assets/Scripts/PlayerController.cs
+++ 2D Game/Assets/Scripts/PlayerController.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+
 	//Player movement variables
 	public float moveSpeed;
 	public float jumpHeight;
+	private bool doubleJump;
+
 	//Player grounded variables
 	private bool grounded;
 	public Transform groundCheck;
 	public float groundCheckRadius;
 	public LayerMask whatIsGround;
 
+	//Non-Slide Player
+	private float moveVelocity; 
+
+	public Animator animator;
 	void Start () {
-	
+		//Animator reset
+		//animator.SetBool("isWalking", false);
+		//animator.SetBool("isJumping", false);
 	}
 
 	void FixedUpdate(){
@@ -21,18 +30,61 @@ public class PlayerController : MonoBehaviour {
 	
 	}
 	void Update () {
-		//Moves player left and right
-		if(Input.GetKey(KeyCode.D)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-		}
-		else if(Input.GetKey(KeyCode.A)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		
+		//Double jump code
+		if(grounded){
+			doubleJump = false;
+			//animator.SetBool("isJumping",false);
 		}
 
+		//Non-Slide Player
+		//moveVelocity = 0f
+
+		if(Input.GetKeyDown (KeyCode.Space) && !doubleJump && !grounded){
+			Jump();
+			doubleJump = true;
+		}
+
+		//Non-Slide Player
+		moveVelocity = 0f;
+
+		//Moves player left and right
+		if(Input.GetKey(KeyCode.D)){
+			// GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			moveVelocity = moveSpeed;
+			//animator.SetBool("isWalking", true);
+		}
+
+		else if(Input.GetKeyUp (KeyCode.D)){
+			// animator.SetBool("isWalking" = false);
+		}
+		
+		if(Input.GetKey(KeyCode.A)){
+			// GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			moveVelocity = -moveSpeed;
+			//animator.SetBool("isWalking", true);
+		}
+
+		else if (Input.GetKeyUp(KeyCode.A)){
+			//animator.SetBool("isWalking" = false);
+		}
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
 		//Makes player jump
+		if(Input.GetKeyDown(KeyCode.Space) && grounded){
+			Jump();
+			//animator.SetBool("isJumping", true);
+		}
+
 		if(Input.GetKeyDown(KeyCode.W) && grounded){
 			Jump();
+			//animator.SetBool("isJumping", true);
 		}
+		
+		// else if(Input.GetKeyUp(KeyCode.W)){
+			//animator.SetBool("isJumping" = false);
+		// }
 
 		//Player Flip
 		if(GetComponent<Rigidbody2D>().velocity.x > 0)
